3bc718ce140539c2721ff49daedd94e150f9933e
David Guentelican <david.guentelican@gmail.com>
2024-09-19 16:05:12 -0300

    creadas las apps core (base de datos) y pagina_web, agregadas al settings.py

---
+++ biblioteca/app_pagina_web/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppPaginaWebConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_pagina_web'
--- biblioteca/biblioteca/settings.py
+++ biblioteca/biblioteca/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_admin'
+    'app_admin',
+    'app_core',
+    'app_pagina_web'
 ]
 
 MIDDLEWARE = [
